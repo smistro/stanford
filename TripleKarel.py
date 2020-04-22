064ee1bee12001e30d013cfa7cd2f72443194517
skunk works <skunkworksbrz@protonmail.ch>
2020-04-22 01:07:42 -0300

    End of exercise

--- TripleKarel.py
+++ TripleKarel.py
@@ -18,69 +18,64 @@ def main():
     starting to write your own code. You should also delete this
     comment and replace it with a better, more descriptive one.
     """
+    block_one()
+
+    while left_is_blocked():
+        put_beeper()
+        move()
+    turn_right()
+
+    block_two()
+
+
+
+
+    """From this point on, the functions used to create the building
+    blocks above will be defined. Awesome. Many thanks to Stanford University
+    for providing me with this unique and challenging learning experience.
+    I'am just grateful for this opportunity."""
+# Defines the most used command by Karel
+def primary_function():
     while left_is_blocked():
         put_beeper()
         move()
     turn_left()
     move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
+
+# Defines secondary command
+def secondary_function():
     while left_is_blocked():
         put_beeper()
         move()
     put_beeper()
     turn_right()
     move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_right()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
+
+# Defines tertiary function
+def tertiary_function():
     while left_is_blocked():
         put_beeper()
         move()
     turn_right()
     put_beeper()
     move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_right()
-    put_beeper()
-    move()
-    while left_is_blocked():
-        put_beeper()
-        move()
-    turn_left()
-    move()
-    
+
+# Defining the building blocks
+def block_one():
+    primary_function()
+    primary_function()
+    secondary_function()
+    primary_function()
+    primary_function()
+
+def block_two():
+    primary_function()
+    primary_function()
+    primary_function()
+    tertiary_function()
+    tertiary_function()
+    primary_function()
+
 
 
 # Turns Karel 90 degrees to the right.
@@ -94,36 +89,6 @@ def turn_around():
     turn_left()
     turn_left()
 
-# Sequence
-def finite_seq():
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-
-# Sequence two
-def finite_seq2():
-    move()
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-    put_beeper()
-
-# Sequence Three
-def finite_seq3():
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-    put_beeper()
-    move()
-
-# Aprendendo Git na prática. 
 
 
     pass
